7ab63c15c4bed9d0eeaccac378652f3cfc0443c0
terencejackson8000 <svensommerfeld1982@gmail.com>
2021-05-15 17:10:46 +0200

    Enums for Logical and Comparison

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+#Call this with python3 example.py <url> <client_id> <client_secret>
 import sys
 
 import constant
@@ -7,14 +7,16 @@ import constant
 from suiteCRMService import SuiteCRMService
 from filter import FilterTriplet
 from filter import Filter
+from filter import Comparison
+from filter import Logical
 
 url = sys.argv[1]
 webService = SuiteCRMService(url, sys.argv[2], sys.argv[3])
 
 response = webService.get_data(constant.OPPORTUNITIES)
 
-filterTriplet = FilterTriplet("GTE", "probability", "75")
-filter = Filter("and", [filterTriplet])
+filterTriplet = FilterTriplet(Comparison.GTE, "probability", "75")
+filter = Filter(Logical.AND, [filterTriplet])
 
 response = webService.get_data(constant.OPPORTUNITIES, ["name", "probability"], filter)
 print (response.json())
--- filter.py
+++ filter.py
@@ -1,35 +1,58 @@
+import enum
+
+class Comparison(enum.Enum):
+   EQ = 1
+   NEQ = 2
+   GT = 3
+   GTE = 4
+   LT = 5
+   LTE = 6
+
+class Logical(enum.Enum):
+    AND = 1
+    OR = 2
+
 class FilterTriplet:
-    #EQ = '=';
-    #NEQ = '<>';
-    #GT = '>';
-    #GTE = '>=';
-    #LT = '<';
-    #LTE = '<=';
+
+    #Constructor for a filter triplet
+    #Parameters
+    #----------
+    #comparison : Comparison
+    #    Comparison type based on the enum Comparison
+    #field : string
+    #   The field for the filter e.g. name
+    #criteria: multiple
+    #   The criteria for the filter e.g. 6 or test
     def __init__(self, comparison, field, criteria):
+        if not isinstance(comparison, Comparison):
+            raise TypeError("comparison parameter has to be of type enum Comparison")
+
         self._comparison = comparison
         self._field = field
         self._criteria = criteria
     
     def to_filter_string(self):
-        return "filter[{0}][{1}]={2}".format(self._field, self._comparison, self._criteria)
+        return "filter[{0}][{1}]={2}".format(self._field, self._comparison.name, self._criteria)
 
 class Filter:
 
     #Constructor for the filter
     #Parameters
     #----------
-    #logical : string
-    #    The logical connector. Must be OR or AND
+    #logical : Logical
+    #    The logical connector. Must be of enum Logical
     #criteria : list
     #   List of criterias of FilterTriplet
     def __init__(self, logical, criteria):
+        if not isinstance(logical, Logical):
+            raise TypeError("logical parameter has to be of type enum Logical")
+
         self._logical = logical
         self._criteria = criteria
     
     def to_filter_string(self):
-        #filter[operator]=and&filter[account_type][eq]=Customer
         criteria_string = ""
         for c in self._criteria:
             criteria_string = "&{0}".format(c.to_filter_string())
 
-        return "filter[operator]={0}{1}".format(self._logical, criteria_string)
+        return "filter[operator]={0}{1}".format(self._logical.name, criteria_string)
